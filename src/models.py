38ac7677c5a85f561335ff778d06baa574482878
yinyang-boop <yinyang@kth.se>
2025-11-01 06:48:50 +0800

    Delete src/models.py

--- src/models.py
+++
@@ -1,5 +0,0 @@
-from sklearn.ensemble import RandomForestClassifier
-def train_rf(X, y, **kwargs):
-    model = RandomForestClassifier(random_state=42, **kwargs)
-    model.fit(X, y)
-    return model
